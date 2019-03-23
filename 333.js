0e84f3d215ab072255402c15f9974e97b086b526
sjs1995 <48668325+sjs1995@users.noreply.github.com>
2019-03-23 13:34:09 +0800

    Add files via upload

---
+++ 333.js
@@ -0,0 +1,14 @@
+Vue.component('blog-post', {
+  props: ['keyy'],
+  template: '<h3>{{ keyy }}</h3>'
+})
+new Vue({
+  el: '#blog-post-demo',
+  data: {
+    posts: [
+      { id: 1, title: 'My journey with Vue' },
+      { id: 2, title: 'Blogging with Vue' },
+      { id: 3, title: 'Why Vue is so fun' }
+    ]
+  }
+})
